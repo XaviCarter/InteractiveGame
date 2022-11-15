b4c2291042581b5f79d1b1cbe4a50d66d4549a51
Xavi Carter <116882813+XaviCarter@users.noreply.github.com>
2022-11-15 10:16:35 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,59 @@
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    background(220);
+    
+
 }
 
-function draw() {
-    background(220);
+var widthRect = 100;
+var heightRect = 100;
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+function draw() {
+    
+
+
+}
+
+function mousePressed() {
+    if(mouseButton == LEFT){
+      fill(255, 255, 255);
+      rectMode(CENTER)
+      rect(mouseX, mouseY, widthRect, heightRect); 
+ 
+    }
+
+    if(mouseButton == RIGHT){
+        fill(255,255,255);
+        ellipseMode(CENTER)
+        ellipse(mouseX, mouseY, widthRect, heightRect);
+    
+    }
+    
+
+   
+
+
+}
+
+function keyPressed() {
+    if(keyCode == ENTER){
+        background(220); 
+    }
+
+    if(keyCode == UP_ARROW){
+        fill(random(0,255), random(0,255), random(0,255))
+        rect(mouseX, mouseY, widthRect+50, heightRect)
+    }
+
+    if(keyCode == DOWN_ARROW){
+        fill('#24FFFF')
+        triangle(mouseX, mouseY, mouseX+100, mouseY, mouseX+100, mouseY-100)
+    }  
+
+    if(keyCode == LEFT_ARROW){
+        fill('#2424FF')
+        rect(mouseX, mouseY, 100, 150)
+        
+    }
 }
